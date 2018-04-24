d1e50baf6eca3c15f2b8bd061e71a848e3a5f2c7
ryuch91 <ryuch91@kaist.ac.kr>
2018-04-24 15:28:43 +0900

    Add parser test to know how rdflib works

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,11 +1,16 @@
-import pronto
+from rdflib import Graph
 
 
 def test_print_ont():
-    ont = pronto.Ontology('https://protege.stanford.edu/ontologies/pizza/pizza.owl')
-    print(ont.json)
+    # ont = pronto.Ontology('https://protege.stanford.edu/ontologies/pizza/pizza.owl')
+    # print(ont.json)
+    # result = g.parse('https://protege.stanford.edu/ontologies/pizza/pizza.owl', format="application/rdf+xml")
+    g = Graph()
+    g.load('./wine.rdf')
+    # g.parse(location='./wine.rdf', format='nt')
+    for s,p,o in g.triples((None, None, None)):
+        print (s,p,o)
 
 
 if __name__ == "__main__":
-    print("hello")
     test_print_ont()
